ee2523999271c0d7970e5ef6ed893b9aa2ac073f
Gabriel <belmonte.gabriel.k@gmail.com>
2014-03-26 17:14:24 -0400

    new test

---
+++ MIDI_CV_firmware/AnalogTests/AnalogTests.cpp
@@ -0,0 +1,18 @@
+/*
+ * AnalogTests.cpp
+ *
+ * Created: 26/03/2014 2:15:44 PM
+ *  Author: zhadowoflight
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ MIDI_CV_firmware/libs/adc.h
@@ -0,0 +1,11 @@
+
+
+#ifndef ADC_LIB
+#define ADC_LIB
+#include "HardAbs.h"
+#include <avr/io.h>
+
+
+
+
+#endif
